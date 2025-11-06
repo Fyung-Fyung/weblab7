470080978851b0392d53ef6c05791f790851740e
JAGDEV <effiongmutanda@gmail.com>
2025-11-06 14:42:50 -0600

    lab7inclass

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function init(){
+//add your javascrip between these two lines of code
+ document.getElementById("myBtn").addEventListener("click", displayDate);
+ alert("Mutanda Effiong"); 
+
+
+
+
+
+
+
+window.addEventListener('load', init);
+}
